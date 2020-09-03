58b5fc30c02c3d76a2550a4cc6ef16c2ec0f658f
Diego Arriola <diego@macbook-pro.lan>
2020-09-02 22:40:42 -0400

    pushing up to assignment 14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,26 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('foo.txt') as foo:
+    for content in foo:
+        print(content)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+print()
+print("----------------------------")
+print()
+
+with open('bar.txt', 'w') as bar:
+    bar.write("This is the first line of text. \n" )
+    bar.write("This is the second line of text. \n")
+    bar.write("This is the third line of text. \n")
+
+
+with open('bar.txt') as bar:
+    for line in bar:
+        print(line)
--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,39 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+today = datetime.now()
+
+def print_calendar(month=today.month, year=today.year):
+  if len(sys.argv) == 1:
+    print(calendar.month(year, month))
+  elif len(sys.argv) == 2:
+    try:
+      print(calendar.month(year, int(sys.argv[1])))
+    except:
+      print('Please enter a valid month integer from 01 to 12')
+  elif len(sys.argv) == 3:
+    try:
+      print(calendar.month(int(sys.argv[2]), int(sys.argv[1])))
+    except:
+      print('Please enter a valid month and year integer ex. MM YYYY')
+  else:
+    print('Plase use the format (14_cal.py MM YYYY)')
+
+
+
+
+if __name__ == "__main__":
+    print_calendar()
